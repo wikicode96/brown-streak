69601128e09d727ba02c3f2694806e2c1913eaca
Carlos Soler García <wikicode96@gmail.com>
2024-03-15 11:26:49 +0100

    :construction: main content of home-page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from './views/home-page/home-page.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
