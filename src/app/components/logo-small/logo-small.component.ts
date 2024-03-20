1ad2a30225dd15a7c1a6aa954bfbbc81f0dfbbd1
Carlos Soler García <101192857+wikicode96@users.noreply.github.com>
2024-03-20 20:01:21 +0100

    :ambulance: paths of images were fixed (#3)
    * :ambulance: incorrect import
    
    * :ambulance: paths of images were fixed

--- src/app/components/logo-small/logo-small.component.ts
+++ src/app/components/logo-small/logo-small.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   imports: [],
   template: `
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <img class="bi me-2" width="40" height="32" src="../../../assets/img/Brown_Streak_Railroad-Logo.webp" alt="Brown Streak Railroad Logo">
+      <img class="bi me-2" width="40" height="32" src="./assets/img/Brown_Streak_Railroad-Logo.webp" alt="Brown Streak Railroad Logo">
     </a>`,
 })
 export class LogoSmallComponent {
