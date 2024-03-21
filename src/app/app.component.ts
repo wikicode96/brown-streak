94acab63d16866e6e56804087fc4c4bf6c6a0f36
Carlos Soler García <wikicode96@gmail.com>
2024-03-21 17:24:44 +0100

    :construction: stations-page in construction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomePageComponent } from './views/home-page/home-page.component'
+import { StationsPageComponent } from './views/stations-page/stations-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomePageComponent],
+  imports: [RouterOutlet, HomePageComponent, StationsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/views/stations-page/stations-page.component.ts
+++ src/app/views/stations-page/stations-page.component.ts
@@ -1,27 +1,22 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/layouts/header/header.component'
 import { SectionComponent } from '../../components/layouts/section/section.component'
-import { MainSearchFormComponent } from '../../components/forms/main-search-form/main-search-form.component'
-import { AlbumComponent } from '../../components/layouts/album/album.component'
 import { FooterComponent } from '../../components/layouts/footer/footer.component'
 import { Heading1Component } from '../../components/fonts/heading1/heading1.component'
 import { Heading2Component } from '../../components/fonts/heading2/heading2.component'
 
 @Component({
-  selector: 'home-page',
+  selector: 'stations-page',
   standalone: true,
   imports: [
     HeaderComponent,
     SectionComponent,
-    MainSearchFormComponent,
-    AlbumComponent,
     FooterComponent,
     Heading1Component,
-    Heading2Component
-  ],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+    Heading2Component],
+  templateUrl: './stations-page.component.html',
+  styleUrl: './stations-page.component.css'
 })
-export class HomePageComponent {
+export class StationsPageComponent {
 
 }
